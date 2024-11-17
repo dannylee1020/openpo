3558ba3b19d31e345fbb54df38cb605169049082
Daniel Lee <dannylee1020@gmail.com>
2024-11-17 15:31:08 -0800

    rename

--- peony/client.py
+++
@@ -1,6 +0,0 @@
-from peony.resources.chat import chat
-
-
-class Peony:
-    def __init__(self):
-        self.chat = chat.Chat()
--- peony/resources/chat/chat.py
+++
@@ -1,6 +0,0 @@
-from peony.resources.chat import completions
-
-
-class Chat:
-    def __init__(self):
-        self.completions = completions.Completions()
