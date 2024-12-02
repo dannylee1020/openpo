86db91c03b166ee29e0ce2226d003ce096c7a76b
Daniel Lee <dannylee1020@gmail.com>
2024-12-01 20:37:11 -0800

    del unused code

--- openpo/resources/chat/chat.py
+++
@@ -1,8 +0,0 @@
-from typing import Union, Dict
-from huggingface_hub import InferenceClient
-from openpo.resources.chat import completions
-
-
-class Chat:
-    def __init__(self, client: Union[InferenceClient, Dict]):
-        self.completions = completions.Completions(client)
